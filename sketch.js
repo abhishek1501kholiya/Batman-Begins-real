c311cff78263b0828e167145ad84925036066631
abhishek1501kholiya <abhishekkholiya15012007@gmail.com>
2020-08-04 10:31:18 +0530

    hello

--- sketch.js
+++ sketch.js
@@ -7,9 +7,14 @@ var engine, world;
 var Drops = [];
 var umberalla;
 var thunder;
+var thunder1 , thunder2;
 var frame = 0;
 function preload() {
   //  backgroundImg = loadImage("sprites/bg.png");
+  thunder1 = loadImage('thunder.png.jpg');
+  thunder2 = loadImage('thunder1.png.jpg');
+ 
+ 
 }
 
 function setup(){
@@ -19,7 +24,7 @@ function setup(){
     for(var i = 0 ; i<100 ; i++){
         Drops.push(new Drop(random(100,400),random(300,600),5,5));
     }
-    thunder = new Thunder(200,600,20,80);
+ //  thunder = new Thunder(200,600,20,80);
 
     umberalla = new Umberella(200,500,20);
 
@@ -37,10 +42,20 @@ function draw(){
         Drops[i].display();
     }
     umberalla.display();
-    frame += 0;
+    if(frameCount %80 ===0){
+        thunders = createSprite(random(50,100),random(0,20));
+        var rand = Math.round(random(1,2));
+         switch(rand){
+            case 1 : image(thunder1,thunders.position.x , thunders.position.y); console.log("Hello");
+            break;
+            case 2 :  image(thunder2,thunders.position.x,thunders.position.y); console.log("buffallo");
+            break;
+            default : break;
+        }
+    }
   //  switch(frame){
     //    case frame => frame%60 ===   0  :
-            thunder.display();
+          //  thunder.display();
       //  case frame => frame%80 === 0 :
        // break ;
     //}
